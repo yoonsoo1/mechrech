517f78a3fd43c32ebdb9d3362fbfa27bf67a3090
Omar Qureshi <owquresh@usc.edu>
2021-12-06 19:44:47 -0800

    Update mechrec.sql

--- mechrec.sql
+++ mechrec.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS Posts (
     carModel VARCHAR(30) NOT NULL,
     carMake VARCHAR(30) NOT NULL,
     carYear INT NOT NULL,
-    img VARCHAR(100),
+    photo mediumblob,
     FOREIGN KEY(CompanyID) REFERENCES Companies(CompanyID),
     PRIMARY KEY (postID)
 );
